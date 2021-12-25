2c2fc9137008f1f653485340d6babcc083eb41d9
ZeinShehab <zeinshehab@outlook.com>
2021-12-26 00:24:27 +0200

    Add additional methods to construct matrices

--- src/org/la/factory/MatrixFactory.java
+++ src/org/la/factory/MatrixFactory.java
@@ -1,6 +1,7 @@
 package org.la.factory;
 
 import org.la.Matrix;
+import org.la.Vector;
 
 // TODO return Matrix ??
 public class MatrixFactory {
@@ -14,7 +15,7 @@ public class MatrixFactory {
 	 * @return 2D array representation of string as matrix
 	 */
 	public static double[][] fromString(String matString) {
-		matString = " " + matString.replace("[", "").replace("]", ""); // add space at [0] for consistency between rows
+		matString = " " + matString.replace("[", "").replace("]", "");
 
 		String[] cols;
 		String[] rows = matString.split(";");
@@ -22,8 +23,8 @@ public class MatrixFactory {
 		double[][] numM = new double[rows.length][];
 
 		for (int i = 0; i < rows.length; i++) {
-			cols = rows[i].substring(1).split(" "); // remove space and split each row into columns
-			numM[i] = new double[cols.length]; // set size of columns according to each column
+			cols = rows[i].substring(1).split(" ");
+			numM[i] = new double[cols.length]; 
 
 			for (int j = 0; j < cols.length; j++)
 				numM[i][j] = Double.parseDouble(cols[j]);
@@ -42,6 +43,50 @@ public class MatrixFactory {
 		return mat;
 	}
 
+	public static double[][] fromConstant(int rows, int cols, double value) {
+		double[][] m = new double[rows][cols];
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; i++)
+				m[i][j] = value;	
+
+		return m;
+	}
+
+	public static double[][] fromConstant(int size, double value) {
+		return fromConstant(size, size, value);
+	}
+
+	public static double[][] fromSize(int rows, int cols) {
+		return new double[rows][cols];
+	}
+
+	public static double[][] fromSize(int size) {
+		return new double[size][size];
+	}
+
+	public static double[][] fromVectorsHorizontal(Vector ... v) {
+		double[][] m = new double[v.length][];
+
+		for (int i = 0; i < v.length; i++) {
+			m[i] = v[i].toArray();
+		}
+		return m;
+	}
+
+	public static double[][] fromVectorsVertical(Vector ... v) {
+		if (v.length == 0 || v[0] == null)
+			throw new IllegalArgumentException("Invalid input size");
+		
+		double[][] m = new double[v[0].length()][v.length];
+
+		for (int i = 0; i < v[0].length(); i++) {
+			for (int j = 0; j < v.length; j++) {
+				m[i][j] = v[j].get(i);
+			}
+		}
+		return m;
+	}
+
 	public static Matrix identity(int size) {
 		double[][] m = new double[size][size];
 		for (int i = 0; i < size; i++) {
@@ -49,6 +94,4 @@ public class MatrixFactory {
 		}
 		return new Matrix(m);
 	}
-
-	// TODO: from constant
 }

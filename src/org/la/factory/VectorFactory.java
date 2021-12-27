0503ec394b184b52431461554f9e6e31acfe6dc9
ZeinShehab <zeinshehab@outlook.com>
2021-12-27 21:16:24 +0200

    Add fromRange methods

--- src/org/la/factory/VectorFactory.java
+++ src/org/la/factory/VectorFactory.java
@@ -2,6 +2,7 @@ package org.la.factory;
 
 import org.la.Linear;
 
+// TODO return Vector??
 public class VectorFactory {
     public static double[] fromArray(double[] arr) {
         double[] res = new double[arr.length];
@@ -18,5 +19,29 @@ public class VectorFactory {
             res[i] = Double.parseDouble(vals[i]);
         }
         return res;
-    } 
+    }
+    
+    public static double[] fromLength(int length) {
+        return new double[length];
+    }
+
+    // TODO: potentially introduce automatic step detection like haskell
+    // accept input of the form [1,2..20] or [1,3..20]
+    public static double[] fromRange(double start, double end, double step) {
+        int size = (int) Math.ceil((end - start) / step);
+        double[] res = new double[size];
+
+        for (int i = 0; i < size; i++)
+            res[i] = start + (i * step);
+        
+        return res;
+    }
+
+    public static double[] fromRange(double start, double end) {
+        return fromRange(start, end, 1);
+    }
+
+    public static double[] fromRange(int end) {
+        return fromRange(0, end, 1);
+    }
 }

5a54bf00865e33ac8d86298749361d65aa991bf7
ZeinShehab <zeinshehab@outlook.com>
2021-12-27 19:55:02 +0200

    Create Linear class

---
+++ src/org/la/Linear.java
@@ -0,0 +1,10 @@
+package org.la;
+
+/**
+ * Contains useful global constants used throughout the project
+ */
+public class Linear {
+    public static final String ROW_DELIMITER = ";";
+    public static final String COL_DELIMITER = " ";
+    public static final double EPS = Math.pow(10, -6);
+}

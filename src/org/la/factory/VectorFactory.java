a8c4c60a35456ccb833a4b6a532a623a25bfd7db
ZeinShehab <zeinshehab@outlook.com>
2021-12-28 17:43:40 +0200

    Methods return Vectors now instead of arrays

--- src/org/la/factory/VectorFactory.java
+++ src/org/la/factory/VectorFactory.java
@@ -1,47 +1,57 @@
 package org.la.factory;
 
 import org.la.Linear;
+import org.la.Vector;
 
-// TODO return Vector??
 public class VectorFactory {
-    public static double[] fromArray(double[] arr) {
-        double[] res = new double[arr.length];
-        System.arraycopy(arr, 0, res, 0, arr.length);
-        return res;
+    // public static double[] fromArray(double[] arr) {
+    //     double[] res = new double[arr.length];
+    //     System.arraycopy(arr, 0, res, 0, arr.length);
+    //     return res;
+    // }
+
+    public static Vector fromArray(double ... arr) {
+        return new Vector(arr);
     }
 
-    public static double[] fromString(String vec) {
+    public static Vector fromString(String vec) {
         vec = vec.replaceAll("\\[|\\]", "");
         String[] vals = vec.split(Linear.COL_DELIMITER);
-        double[] res = new double[vals.length];
+        Vector res = fromLength(vals.length);
 
         for (int i = 0; i < vals.length; i++) {
-            res[i] = Double.parseDouble(vals[i]);
+            res.set(i, Double.parseDouble(vals[i]));
         }
         return res;
     }
     
-    public static double[] fromLength(int length) {
-        return new double[length];
+    public static Vector fromLength(int length) {
+        return new Vector(new double[length]);
     }
 
     // TODO: potentially introduce automatic step detection like haskell
-    // accept input of the form [1,2..20] or [1,3..20]
-    public static double[] fromRange(double start, double end, double step) {
+    // accept input of the form [1,2..20] or [1,3..20] .split(,|..)
+    public static Vector fromRange(double start, double end, double step) {
         int size = (int) Math.ceil((end - start) / step);
-        double[] res = new double[size];
+        Vector res = fromLength(size);
 
         for (int i = 0; i < size; i++)
-            res[i] = start + (i * step);
+            res.set(i, start + (i * step));
         
         return res;
     }
 
-    public static double[] fromRange(double start, double end) {
+    public static Vector fromRange(double start, double end) {
         return fromRange(start, end, 1);
     }
 
-    public static double[] fromRange(int end) {
+    public static Vector fromRange(int end) {
         return fromRange(0, end, 1);
+    }
+
+    public static Vector fromConstant(int length, double value) {
+        Vector res = fromLength(length);
+        res.setAll(value);
+        return res;
     }
 }

98d6c169e4d546bc6dc883dcccb2031be622468b
ZeinShehab <zeinshehab@outlook.com>
2022-01-01 22:54:54 +0200

    EPS is now a constant

--- src/org/la/Linear.java
+++ src/org/la/Linear.java
@@ -4,7 +4,8 @@ package org.la;
  * Contains useful global constants used throughout the project
  */
 public class Linear {
-    public static final String ROW_DELIMITER = ";";
-    public static final String COL_DELIMITER = " ";
-    public static final double EPS = Math.pow(10, -6);
+    public static final String ROW_DELIMITER   = ";";
+    public static final String COL_DELIMITER   = " ";
+    
+    public static final double EPS = 1.0E-6;
 }

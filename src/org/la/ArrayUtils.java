ba8f38db02870a7f9c4c7f0d9a82149535a83c15
ZeinShehab <zeinshehab@outlook.com>
2022-01-02 15:56:35 +0200

    Add new utility methods

--- src/org/la/ArrayUtils.java
+++ src/org/la/ArrayUtils.java
@@ -21,4 +21,61 @@ public class ArrayUtils {
         copy(src, res);
         return res;
     }
+
+    public static void shl(double[] arr, double val) {
+        for (int i = 1; i < arr.length; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr[arr.length-1] = val;
+    }
+
+    public static void shr(double[] arr, double val) {
+        for (int i = arr.length - 1; i > 0; i++) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = val;
+    }
+
+    public static void copyElements(double[] src, double[] dest) {
+        for (int i = 0; i < dest.length && i < src.length; i++) {
+            dest[i] = src[i];
+        }
+    }
+
+    public static double[] expand(double[] src, int expansion) {
+        double[] res = new double[src.length + expansion];
+        copyElements(src, res);
+        return res;
+    }
+
+    public static double[] shrink(double[] src, int shrink) {
+        double[] res = new double[src.length - shrink];
+        copyElements(src, res);
+        return res;
+    }
+
+    public static double[] remove(double[] src, int index) {
+        double[] res = new double[src.length - 1];
+        for (int i = 0; i < index; i++) 
+            res[i] = src[i];
+
+        for (int i = index; i < res.length; i++)
+            res[i] = src[i+1];
+
+        return res;
+    }
+
+    public static double[] insert(double[] src, double value, int index) {
+        double[] res = new double[src.length + 1];
+
+        for (int i = 0; i < index; i++)
+            res[i] = src[i];
+
+        res[index] = value;
+
+        for (int i = index + 1; i < res.length; i++)
+            res[i] = src[i-1];
+
+        return res;
+    }
 }

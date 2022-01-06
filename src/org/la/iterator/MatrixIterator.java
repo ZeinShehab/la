10a38805cbb213f92c0a6568d4b9eed0bd043b52
ZeinShehab <zeinshehab@outlook.com>
2022-01-06 16:17:25 +0200

    Add documentation for iterators

--- src/org/la/iterator/MatrixIterator.java
+++ src/org/la/iterator/MatrixIterator.java
@@ -3,13 +3,28 @@ package org.la.iterator;
 import java.util.Iterator;
 
 public abstract class MatrixIterator implements Iterator<Double> {
+    /**
+     * Default constructor
+     */
     public MatrixIterator() {};
 
-    public abstract double get();
-
+    /**
+     * Returns the index of the current row
+     */
     public abstract int rowIndex();
 
+    /**
+     * Returns the index of the current column
+     */
     public abstract int colIndex();
 
+    /**
+     * Returns the value at the current index
+     */
+    public abstract double get();
+
+    /**
+     * Set the value at the current index to {@code value}
+     */
     public abstract void set(double value); 
 }
--- src/org/la/iterator/VectorIterator.java
+++ src/org/la/iterator/VectorIterator.java
@@ -3,11 +3,23 @@ package org.la.iterator;
 import java.util.Iterator;
 
 public abstract class VectorIterator implements Iterator<Double> {
+    /**
+     * Default constructor
+     */
     public VectorIterator() {}
 
+    /**
+     * Returns the current index
+     */
     public abstract int index();
 
+    /**
+     * Retuns the value at the current index
+     */
     public abstract double get();
     
+    /**
+     * Sets the value at the current index to {@code value} 
+     */
     public abstract void set(double value);
 }

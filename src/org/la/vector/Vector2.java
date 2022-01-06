660aa4acf0d0538361d7c2e75999d4a0cad84273
ZeinShehab <zeinshehab@outlook.com>
2022-01-06 16:11:40 +0200

    Add documentation for Vector2 and Vector3

--- src/org/la/vector/Vector2.java
+++ src/org/la/vector/Vector2.java
@@ -2,11 +2,24 @@ package org.la.vector;
 
 import org.la.Vector;
 
+/**
+ * A 2D vector is represented by an array of 2 real numbers. It has a fixed size.
+ * @see org.la.Vector
+ */
 public class Vector2 extends Vector {
+    /**
+     * Creates a 2D vector from {@code x} and {@code y}
+     */
     public Vector2(double x, double y) {
         super(x, y);
     }    
 
+    /**
+     * Creates a 2D vector from {@code v} while copying the elements
+     * <p>If the length of {@code v} is not 2, an {@code IllegalArgumentException}
+     * will be thrown</p>
+     * @throws IllegalArgumentException
+     */
     public Vector2(double[] v) {
         super(v);
 
@@ -15,20 +28,38 @@ public class Vector2 extends Vector {
         }
     }
 
+    /**
+     * Creates a 2D vector from {@code v}.
+     * <p> If {@code v} has more than 2 components then they are ignored
+     * and only the first 2, x and y, are taken </p>
+     * <p>This constructor does not handle the case 
+     * when the size of {@code v} is less than 2 and it will fail</p>
+     */
     public Vector2(Vector v) {
         this(v.get(0), v.get(1));
     }
 
+    /**
+     * Returns the X component of the vector
+     */
     public double getX() {
         return get(0);
     }
 
+    /**
+     * Returns the Y component of the vector
+     */
     public double getY() {
         return get(1);
     }
 
-    public Vector3 cross(Vector v) {
-        Vector2 v2 = new Vector2(v);
+    /**
+     * Returns the cross product of vector with {@code u}
+     * @param u a vector
+     * @return v x u
+     */
+    public Vector3 cross(Vector u) {
+        Vector2 v2 = new Vector2(u);
         return new Vector3(0, 0, getX()*v2.getY() - v2.getX()*getY());
     }
 }
--- src/org/la/vector/Vector3.java
+++ src/org/la/vector/Vector3.java
@@ -2,11 +2,24 @@ package org.la.vector;
 
 import org.la.Vector;
 
+/**
+ * A #D vector is represented by an array of 3 real numbers. It has a fixed size.
+ * @see org.la.Vector
+ */
 public class Vector3 extends Vector {
+    /**
+     * Creates a 3D vector from {@code x}, {@code y} and {@code z}
+     */
     public Vector3(double x, double y, double z) {
         super(x, y, z);
     }
 
+    /**
+     * Creates a 3D vector from {@code v} while copying the elements
+     * <p>If the length of {@code v} is not 3, an {@code IllegalArgumentException}
+     * will be thrown</p>
+     * @throws IllegalArgumentException
+     */
     public Vector3(double[] v) {
         super(v);
 
@@ -14,31 +27,59 @@ public class Vector3 extends Vector {
             throw new IllegalArgumentException("Invalid size for 3D Vector");
     }
 
+    /**
+     * Creates a 3D vector from {@code v}.
+     * <p> If {@code v} has more than 3 components then they are ignored
+     * and only the first 3, x,y and z, are taken </p>
+     * <p>This constructor does not handle the case 
+     * when the size of {@code v} is less than 3 and it will fail</p>
+     */
     public Vector3(Vector v) {
         this(v.get(0), v.get(1), v.get(2));
     }   
 
+    /**
+     * Returns the X component of the vector
+     */
     public double getX() {
         return get(0);
     }
 
+    /**
+     * Returns the Y component of the vector
+     */
     public double getY() {
         return get(1);
     }
 
+    /**
+     * Returns the Z component of the vector
+     */
     public double getZ() {
         return get(2);
     }
 
-    public Vector3 cross(Vector v) {
-        Vector3 v3 = new Vector3(v);
+    /**
+     * Returns the cross product of vector with {@code u}
+     * @param u a vector
+     * @return v x u
+     */
+    public Vector3 cross(Vector u) {
+        Vector3 v3 = new Vector3(u);
         return new Vector3( getY() * v3.getZ() - getZ() * v3.getY(),
                             getZ() * v3.getX() - getX() * v3.getZ(),
                             getX() * v3.getY() - getY() * v3.getX());
     }
 
-    public double scalarTripleProduct(Vector v, Vector u) {
-        Vector3 v3 = new Vector3(v);
-        return this.dot(v3.cross(u));
+    /**
+     * Returns the scalar triple product of the vector {@code v} with
+     * {@code u} and {@code w} 
+     * @param u 2nd vector
+     * @param w 3rd vector
+     * @return v dot (u cross w)
+     */
+    public double tripleProduct(Vector u, Vector w) {
+        Vector3 v3 = new Vector3(u);
+        return this.dot(v3.cross(w));
     }
 }
